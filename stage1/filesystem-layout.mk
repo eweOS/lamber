1f85242d1521d2ad529d5178bfdb281eb6681998
Yao Zi <ziyao@disroot.org>
2025-10-19 09:21:36 +0000

    stage1/filesystem-layout: Create /dev mountpoint
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage1/filesystem-layout.mk
+++ stage1/filesystem-layout.mk
@@ -6,7 +6,7 @@ $W/stage1.filesystem-layout:
 	ln -s usr/bin		$O/sbin
 	ln -s bin		$O/usr/sbin
 
-	install -dm 755 "$O"/{home,mnt,srv,run,opt}
+	install -dm 755 "$O"/{home,mnt,srv,run,opt,dev}
 	install -dm 750 "$O"/root
 	install -dm 555 "$O"/{proc,sys}
 	install -dm 1777 "$O"/tmp
