2b9554f44c1d2d4d85c88ca82c9600b4b80b2032
Yao Zi <ziyao@disroot.org>
2025-10-19 12:35:08 +0000

    stage2/automake: New recipe
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ stage2/automake.mk
@@ -0,0 +1,11 @@
+$W/stage2.automake.extract: $W/AUTOMAKE.download
+	tar xf $W/$(AUTOMAKE_FILE) -C $W
+	$(call done)
+
+$W/stage2.automake: $W/stage2.automake.extract $W/stage2.perl
+	cd $W/automake-$(AUTOMAKE_V) && ./configure --prefix=/usr
+
+	make -C $W/automake-$(AUTOMAKE_V)
+	make -C $W/automake-$(AUTOMAKE_V) install
+
+	$(call done)
