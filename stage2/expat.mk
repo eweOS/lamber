2ad96be8421531edc4232a5ebe83ac3434245fbc
Yao Zi <ziyao@disroot.org>
2025-10-22 14:55:18 +0000

    stage2/expat: New recipe
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ stage2/expat.mk
@@ -0,0 +1,15 @@
+$W/stage2.expat.extract: $W/EXPAT.download
+	tar xf $W/$(EXPAT_FILE) -C $W
+
+	$(call done)
+
+$W/stage2.expat: $W/stage2.expat.extract $W/stage2.cmake
+	cmake -S $W/expat-$(EXPAT_V) -B $W/build-expat \
+		-DCMAKE_INSTALL_PREFIX=/usr	\
+		-DCMAKE_BUILD_TYPE=None		\
+		-W no-dev
+
+	cmake --build $W/build-expat
+	cmake --install $W/build-expat
+
+	$(call done)
