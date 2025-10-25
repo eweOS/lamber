ee52ee37aca8559c543187b7e627018b88791e91
Yao Zi <ziyao@disroot.org>
2025-10-25 10:48:20 +0000

    stage2/automake: Depend on autoconf
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage2/automake.mk
+++ stage2/automake.mk
@@ -2,7 +2,8 @@ $W/stage2.automake.extract: $W/AUTOMAKE.download
 	tar xf $W/$(AUTOMAKE_FILE) -C $W
 	$(call done)
 
-$W/stage2.automake: $W/stage2.automake.extract $W/stage2.perl
+$W/stage2.automake: $W/stage2.automake.extract \
+	$W/stage2.perl $W/stage2.autoconf
 	cd $W/automake-$(AUTOMAKE_V) && ./configure --prefix=/usr
 
 	+ make -C $W/automake-$(AUTOMAKE_V)
