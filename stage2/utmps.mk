beae0a877b88bffe7a8b52d6b05bc47fc81be3b6
Yao Zi <ziyao@disroot.org>
2025-10-26 14:20:12 +0000

    stage2/utmps: New recipe
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ stage2/utmps.mk
@@ -0,0 +1,19 @@
+$W/stage2.utmps.extract: $W/UTMPS.download
+	tar xf $W/$(UTMPS_FILE) -C $W
+	$(call done)
+
+$W/stage2.utmps: $W/stage2.utmps.extract
+	cd $W/utmps-$(UTMPS_V) && ./configure --prefix=/usr \
+		--bindir=/usr/bin				\
+		--libdir=/usr/lib				\
+		--with-sysdeps=/usr/lib/skalibs/sysdeps		\
+		--enable-libc-includes				\
+		--enable-shared					\
+		--disable-allstatic				\
+		--enable-utmp					\
+		--enable-pkgconfig
+
+	+ make -C $W/utmps-$(UTMPS_V)
+	+ make -C $W/utmps-$(UTMPS_V) install
+
+	$(call done)
