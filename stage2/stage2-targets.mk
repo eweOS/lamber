32fdd9a3c6cf591b4dde6b5e5d3edda2e47abe42
Yao Zi <ziyao@disroot.org>
2025-12-07 10:21:24 +0000

    stage2: Add target for a full stage2 build
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage2/stage2-targets.mk
+++ stage2/stage2-targets.mk
@@ -10,3 +10,12 @@ $W/stage2.base-devel: \
 	$W/stage2.gettext-tiny $W/stage2.cmake $W/stage2.expat
 
 	$(call done)
+
+$W/stage2: \
+	$W/stage2.fakeroot $W/stage2.file $W/stage2.gettext-tiny	\
+	$W/stage2.gnupg $W/stage2.gpgme $W/stage2.pacman		\
+	$W/stage2.libassuan $W/stage2.libbz2 $W/stage2.libffi 		\
+	$W/stage2.libgcrypt $W/stage2.libgpg-error $W/stage2.libksba	\
+	$W/stage2.libudev-zero $W/stage2.lz4 $W/stage2.npth		\
+	$W/stage2.pam $W/stage2.pinentry $W/stage2.skalibs		\
+	$W/stage2.util-linux $W/stage2.utmps $W/stage2.base-devel
