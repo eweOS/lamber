66864c39bc5bd37cae919397942638fab81fe4d6
Yao Zi <ziyao@disroot.org>
2025-12-07 16:56:36 +0000

    stage2/utmps: Fix dependency on skalibs
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage2/utmps.mk
+++ stage2/utmps.mk
@@ -2,7 +2,7 @@ $W/stage2.utmps.extract: $W/UTMPS.download
 	tar xf $W/$(UTMPS_FILE) -C $W
 	$(call done)
 
-$W/stage2.utmps: $W/stage2.utmps.extract
+$W/stage2.utmps: $W/stage2.utmps.extract $W/stage2.skalibs
 	cd $W/utmps-$(UTMPS_V) && ./configure --prefix=/usr \
 		--bindir=/usr/bin				\
 		--libdir=/usr/lib				\
