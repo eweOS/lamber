48b5cd5233b312a38187e1c1f7c043564d968766
Yao Zi <me@ziyao.cc>
2026-01-14 05:06:17 +0000

    stage2/flex: Manually update config.{sub,guess}
    As for flex 2.6.4, the scripts shipped in tarball couldn't detect
    loongarch64.
    
    Signed-off-by: Yao Zi <me@ziyao.cc>

--- stage2/flex.mk
+++ stage2/flex.mk
@@ -3,7 +3,10 @@ $W/stage2.flex.extract: $W/FLEX.download
 
 	$(call done)
 
-$W/stage2.flex: $W/stage2.flex.extract $W/stage2.bison
+$W/stage2.flex: $W/stage2.flex.extract $W/stage2.bison $W/stage2.autoconf
+	cp /usr/share/autoconf/build-aux/config.{guess,sub} \
+		$W/flex-$(FLEX_V)/build-aux
+
 	cd $W/flex-$(FLEX_V) && ./configure --prefix=/usr
 
 	+ make -C $W/flex-$(FLEX_V)
